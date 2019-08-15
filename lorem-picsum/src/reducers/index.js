7bf53dddb2fe473decd8ebce1b0b595b02044330
Red <vhanchar@ucsc.edu>
2019-08-15 13:53:13 -0700

    created a moch reducer and set up the store

---
+++ lorem-picsum/src/reducers/index.js
@@ -0,0 +1,12 @@
+const initialState = {
+    temp: 'temp value'
+}
+
+const reducer = (state = initialState, action) =>{
+    switch(action.type){
+        default:
+            return state;
+    }
+}
+
+export default reducer;
